8f825cc2c03297ef79f6b1a7f3ed2650deb504d3
nuraansa <nuraansa@gmail.com>
2023-06-28 14:33:02 +0200

    update

---
+++ vue_js_eomp/src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+         <!-- nav -->
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"
+            ><img
+              loading="lazy"
+              src="https://i.postimg.cc/DwRsRxz5/Screenshot-2023-06-12-103831.png"
+              alt="logo"
+              width="75px"
+          /></a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-underline">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="./index.html"
+                  >Home</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./html/products.html">Products</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./html/checkout.html">Checkout</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./html/admin.html">Admin</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link contact" href="./html/contact.html">Contact</a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control m-2 sea" type="search" placeholder="Search Something" aria-label="Search">
+              <button class="btn btn-outline-dark m-2 sea" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+git 
+<style scoped>
+
+</style>
---
+++ vue_js_eomp/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Home from '@/components/Home'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Home
+  }
+}
+</script>
